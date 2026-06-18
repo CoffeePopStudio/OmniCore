51921e32d9e62c231def9e0ed978a3636cc44b20
oneachina <ruiruixi@hotmail.com>
2026-06-18 23:08:29 +0800

    feat: add auto-imports type definitions and PreviewDetail component
    Signed-off-by: oneachina <ruiruixi@hotmail.com>

---
+++ web-frontend/src/components/PreviewDetail.vue
@@ -0,0 +1,15 @@
+<template>
+  <n-scrollbar style="max-height: 400px">
+    <n-list bordered>
+      <n-list-item v-for="(detail, location, index) in preview" :key="index">
+        <n-thing :title="location" :description="detail" />
+      </n-list-item>
+    </n-list>
+  </n-scrollbar>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  preview: Record<string, string>
+}>()
+</script>
